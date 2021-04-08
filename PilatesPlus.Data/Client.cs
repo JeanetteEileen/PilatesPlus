cd7e1017e38155e818a31f1751df895bdf9bfd78
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-04-08 19:08:32 -0400

    Add Client.cs to Data layer

---
+++ PilatesPlus.Data/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilatesPlus.Data
+{
+    public class Client
+    {
+        [Key]
+        public int clientId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string cellPhone { get; set; }
+
+    }
+}
