a238017c915760a3d0a4290f1c90cd6540c6b44c
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-04-08 21:15:57 -0400

    Add ClientController, ActionResult Index(), private ClientService CreateClientService()

---
+++ PilatesPlus.WebMVC/Controllers/ClientController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using PilatesPlus.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PilatesPlus.WebMVC.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            var service = CreateClientService();
+            var model = service.GetClients();
+
+            return View();
+        }
+        private ClientService CreateClientService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new ClientService(userId);
+            return service;
+        }
+    }
+}
