a359cf2ab7ff4ae63697b5dfa6172dd68993b6ff
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-04-08 21:32:39 -0400

    add Cllient Create.cshtml and modify so the App reflects PilatesPlus

--- PilatesPlus.WebMVC/Controllers/ClientController.cs
+++ PilatesPlus.WebMVC/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity;
+using PilatesPlus.Models;
 using PilatesPlus.Services;
 using System;
 using System.Collections.Generic;
@@ -18,11 +19,35 @@ namespace PilatesPlus.WebMVC.Controllers
 
             return View();
         }
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ClientCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateClientService();
+            
+            if (service.CreateClient(model))
+            {
+                TempData["SaveResult"] = "Your client was created.";
+                return RedirectToAction("Index");
+            };
+            ModelState.AddModelError("", "Your client could not be created.");
+            return View(model);
+        }
+
+        // GET: helper method
         private ClientService CreateClientService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new ClientService(userId);
             return service;
         }
+        
     }
 }
