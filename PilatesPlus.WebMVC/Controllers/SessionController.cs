d824aad43d88a90ee2f602978d71f01f1038b69c
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-04-15 14:42:40 -0400

    add Get for Index,  Create for SessionCreate, SessionService helper method

--- PilatesPlus.WebMVC/Controllers/SessionController.cs
+++ PilatesPlus.WebMVC/Controllers/SessionController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using PilatesPlus.Models;
+using PilatesPlus.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,37 @@ namespace PilatesPlus.WebMVC.Controllers
         // GET: Session
         public ActionResult Index()
         {
+            var service = CreateSessionService();
+            var model = service.GetSessions();
+            return View(model);
+        }
+        public ActionResult Create()
+        {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SessionCreate model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateSessionService();
+
+            if (service.CreateSession(model))
+            {
+                TempData["SaveResult"] = "Your Session was created";
+                return RedirectToAction("Index");
+            };
+            ModelState.AddModelError("", "Your Session could not be created. Please review your entries.");
+            return View(model);
+        }
+
+        // GET: helper method
+        private SessionService CreateSessionService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new SessionService(userId);
+            return service;
         }
     }
 }
