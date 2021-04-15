a9b1a704b82c853d49c1291efc5a3564edb1c8ab
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-04-15 12:38:00 -0400

    Add SessionListItem

---
+++ PilatesPlus.Models/SessionListItem.cs
@@ -0,0 +1,29 @@
+﻿using PilatesPlus.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilatesPlus.Models
+{
+    public class SessionListItem
+    {
+        [ForeignKey(nameof(Client))]
+        public int ClientId { get; set; }
+        public virtual Client client { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Display(Name = "Session Date")]
+        public DateTime SessionDate { get; set; }
+        [Display(Name ="Session Notes")]
+        public string SessionNote { get; set; }
+        public bool IsDuet { get; set; }
+    }
+}
