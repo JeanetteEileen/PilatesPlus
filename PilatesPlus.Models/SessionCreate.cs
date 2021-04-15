2348234dcb0712db22f1b96713cbff938a2d8a6f
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-04-15 12:36:58 -0400

    add SessionCreate

---
+++ PilatesPlus.Models/SessionCreate.cs
@@ -0,0 +1,35 @@
+﻿using PilatesPlus.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilatesPlus.Models
+{
+    public class SessionCreate
+    {
+        [Key]
+        public int SessionId { get; set; }
+        [Required]
+        [ForeignKey(nameof(client))]
+        public int ClientId { get; set; }
+        public virtual Client client { get; set; }
+        [Required]
+        [Display(Name ="Client First Name")]
+        public int FirstName { get; set; }
+        [Required]
+        [Display(Name = "Client Last Name")]
+        public int LastName { get; set; }
+        [Required]
+        [Display(Name = "Session Comments")]
+        public string SessionNote { get; set; }
+        [Display(Name ="Is session duet?")]
+        public bool IsDuet { get; set; }
+        [Required]
+        [Display(Name = "Session Date")]
+        public DateTime SessionDate { get; set; }
+    }
+}
