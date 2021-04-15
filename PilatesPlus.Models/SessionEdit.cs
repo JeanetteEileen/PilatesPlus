ce2e77aabc428c583fc515a92324d7748c5df217
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-04-15 12:37:39 -0400

    Add SessionEdit

---
+++ PilatesPlus.Models/SessionEdit.cs
@@ -0,0 +1,39 @@
+﻿using PilatesPlus.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilatesPlus.Models
+{
+    public class SessionEdit
+    {
+        [Key]
+        public int SessionId { get; set; }
+        [Required]
+        [ForeignKey(nameof(client))]
+        public int ClientId { get; set; }
+        public virtual Client client { get; set; }
+        [Required]
+        [Display(Name = "Client First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Client Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Session Comments")]
+        public string SessionNote { get; set; }
+        [Display(Name = "Is session duet?")]
+        public bool IsDuet { get; set; }
+        [Required]
+        [Display(Name = "Session Date")]
+        public DateTime SessionDate { get; set; }
+        [Display(Name = "Date Session Modified")]
+        public DateTimeOffset? SessionDateModified { get; set; }
+
+
+    }
+}
