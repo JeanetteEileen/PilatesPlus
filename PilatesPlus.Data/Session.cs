4433f332930d5f4a1fa63f010a3a789f9f4ced3c
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-04-15 12:34:42 -0400

    Add Session.cs

---
+++ PilatesPlus.Data/Session.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilatesPlus.Data
+{
+    public class Session
+    {
+        [Key]
+        public int SessionId { get; set; }
+        [Required]
+        public Guid OwnerId { get; set; }
+        [Required]
+        [ForeignKey(nameof(client))]
+        public int ClientId { get; set; }
+        public virtual Client client { get; set; }
+        [Display(Name ="First Name")]
+        [Required]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+        [Display(Name = "Session Comments")]
+        [Required]
+        public string SessionNote { get; set; }
+        [Display(Name = "Session Date")]
+        public DateTime SessionDate { get; set; }
+        [Display(Name ="Is Session Duet")]
+        public bool IsDuet { get; set; }
+        [Display(Name = "Date Session Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Date Session Modified")]
+        public DateTimeOffset? SessionDateModified { get; set; }
+    }
+}
