19478c459c20fe1c9218034792232507f62bd51f
JenaetteEileen <jeanetteeileen787@gmail.com>
2021-05-02 08:09:49 -0400

    Took out the Client foreign key again

--- PilatesPlus.Models/EquipmentDetail.cs
+++ PilatesPlus.Models/EquipmentDetail.cs
@@ -15,9 +15,9 @@ namespace PilatesPlus.Models
         [Display(Name ="Equipment")]
         public int EquipmentSessionId { get; set; }
         public virtual Session Session { get; set; }
-        [ForeignKey(nameof(Client))]
-        public int ClientId { get; set; }
-        public virtual Client Client { get; set; }
+        //[ForeignKey(nameof(Client))]
+        //public int ClientId { get; set; }
+        //public virtual Client Client { get; set; }
         public bool Reformer { get; set; }
         public bool Cadilac { get; set; }
         public bool Mat { get; set; }
